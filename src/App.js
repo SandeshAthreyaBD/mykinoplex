350f364813c147e81ba2c07fa67b6d0233cb0541
SandeshAthreyaBD <34373336+SandeshAthreyaBD@users.noreply.github.com>
2019-02-10 21:43:41 +0100

    Navbar is updated and basic routes are done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
-import { MDBContainer } from "mdbreact";
-import Navbar from "./Navbar";
-import "./index.css";
+import Navbar from "./Components/Navbar";
+import { Route, BrowserRouter } from "react-router-dom";
+import Login from "./Components/Login";
+import Kannada from "./Components/Languages/Kannada";
+import Tamil from "./Components/Languages/Tamil";
+import Telugu from "./Components/Languages/Telugu";
+import Hindi from "./Components/Languages/Hindi";
+import Malayalam from "./Components/Languages/Malayalam";
+import Home from "./Components/Home";
+import Contact from "./Components/Contact";
 
 class App extends Component {
   render() {
     return (
-      <MDBContainer fluid>
-        <Navbar />
-      </MDBContainer>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Home} />
+          <Route path="/kannada" component={Kannada} />
+          <Route path="/tamil" component={Tamil} />
+          <Route path="/hindi" component={Hindi} />
+          <Route path="/telugu" component={Telugu} />
+          <Route path="/malayalam" component={Malayalam} />
+          <Route path="/login" component={Login} />
+          <Route path="/contact" component={Contact} />
+        </div>
+      </BrowserRouter>
     );
   }
 }
