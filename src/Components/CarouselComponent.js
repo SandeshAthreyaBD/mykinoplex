bf96b0384e47bd92ba821d97c83a11120454cd81
Preethi-V-3 <20477482+Preethi-V-3@users.noreply.github.com>
2019-02-24 00:01:03 +0100

    carousel changed to react-slick

--- src/Components/CarouselComponent.js
+++ src/Components/CarouselComponent.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import Coverflow from "react-coverflow";
-import { StyleRoot } from "radium";
-
+import Slider from "react-slick";
+import Carousel from 'react-bootstrap/Carousel'
+import {StyleRoot} from "radium";
+import { MDBContainer } from "mdbreact";
 class CarouselComponent extends Component {
   constructor(props) {
     super(props);
@@ -10,121 +11,57 @@ class CarouselComponent extends Component {
       active: 0
     };
   }
-  _handleClick() {
-    var num = Math.floor(Math.random() * 10 + 1);
-    this.setState({
-      active: num
-    });
-  }
 
   render() {
+    const settings = {
+      className: "center",
+      centerMode: true,
+      centerPadding: "50px",
+      slidesToShow: 3,
+      // focusOnSelect: true,
+      // autoplaySpeed: 2000,
+      // pauseOnHover: true,
+      // autoplay: true,
+    };
+
     return (
-      <StyleRoot>
-        <div
-          media={{
-            "@media (max-width: 900px)": {
-              width: "600px",
-              height: "300px"
-            },
-            "@media (min-width: 900px)": {
-              width: "960px",
-              height: "600px"
-            }
-          }}
-          style={{ backgroundColor: "#1C2331" }}
-        >
-          <Coverflow
-            width={960}
-            height={470}
-            displayQuantityOfSide={3}
-            // navigation={true}
-            enableHeading={false}
-            active={this.state.active}
-            infiniteScroll={true}
-            style={{ backgroundColor: "black" }}
-          >
-            <div
-              // onClick={}
-              // onKeyDown={}
-              role="menuitem"
-              tabIndex="0"
-              style={{ backgroundColor: "black" }}
-            >
-              {/* <img src={require("../Images/album-1.png")} alt="Album one" /> */}
-            </div>
-            <img
-              src={require("../Images/aquaman.jpg")}
-              alt="Album two"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/Gully.jpg")}
-              alt="Album two"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/kgf.jpg")}
-              alt="Album two"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/manikarnika.jpg")}
-              alt="Album two"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/uri.jpg")}
-              alt="Album two"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/aquaman.jpg")}
-              alt="Album three"
-              data-action="https://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/Gully.jpg")}
-              alt="Album four"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/kgf.jpg")}
-              alt="Album five"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/manikarnika.jpg")}
-              alt="Album six"
-              data-action="https://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/uri.jpg")}
-              alt="Album seven"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/aquaman.jpg")}
-              alt="Album one"
-              data-action="https://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/Gully.jpg")}
-              alt="Album two"
-              data-action="http://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/kgf.jpg")}
-              alt="Album three"
-              data-action="https://www.mykinoplex.com"
-            />
-            <img
-              src={require("../Images/manikarnika.jpg")}
-              alt="Album four"
-              data-action="http://www.mykinoplex.com"
-            />
-          </Coverflow>
-        </div>
-      </StyleRoot>
+            <Slider {...settings}>
+            <Carousel.Item>
+          <img
+            className="d-block w-100 carousel"
+            src={require("../Images/aquaman.jpg")}
+            alt="First slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 carousel"
+            src={require("../Images/Gully.jpg")}
+            alt="Second slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 carousel"
+            src={require("../Images/kgf.jpg")}
+            alt="Third slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 carousel"
+            src={require("../Images/manikarnika.jpg")}
+            alt="Fourth slide"
+          />
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100 carousel"
+            src={require("../Images/uri.jpg")}
+            alt="Fifth slide"
+          />
+        </Carousel.Item>
+            </Slider>
     );
   }
 }
