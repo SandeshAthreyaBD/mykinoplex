da411cd6b45cd3c76cd660828758acfb0cdd6ab9
Preethi-V-3 <20477482+Preethi-V-3@users.noreply.github.com>
2019-03-16 03:55:29 +0100

    docker files update seperate instances in aws URL set

--- client/src/Constants.js
+++ client/src/Constants.js
@@ -1,6 +1,7 @@
 export const SERVER_PORT = ":3001";
 export const CLIENT_PORT = ":3000";
-export const SERVER_URL = "http://ec2-54-93-63-36.eu-central-1.compute.amazonaws.com"; 
+export const CLIENT_URL = "http://ec2-54-93-63-36.eu-central-1.compute.amazonaws.com"; 
 export const LOCALHOST_URL = "http://localhost";
+export const SERVER_URL = "http://ec2-52-59-253-197.eu-central-1.compute.amazonaws.com";
 
 export const URL_TO_USE = SERVER_URL + SERVER_PORT;
